83d793ae695f7456cf6a092cb335a7a8b923f362
Adilson Enio Pierog <dirsors@gmail.com>
2022-12-20 00:34:31 -0300

    Update d_main.c
    these includes are necessary for compilation and it'll avoid errors like: "d_main.c:42:45: erro: unknown type name ‘u32’"

--- d_main.c
+++ d_main.c
@@ -33,6 +33,12 @@ static const char rcsid[] = "$Id: d_main.c,v 1.8 1997/02/03 22:45:09 b1 Exp $";
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <tamtypes.h>
+#include <sifcmd.h>
+#include <kernel.h>
+#include <stdarg.h>
+#include <debug.h>
+#include <m_menu.h>
 
 /// cosmito
 static char padBuf[256] __attribute__((aligned(64)));
